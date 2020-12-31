ee8a358f0d5991ccdaf29ee06f749b1265c0be45
BuildTools <unconfigured@null.spigotmc.org>
2020-12-31 14:42:41 +0000

    Add create script for letsencrypt to swag change

---
+++ scripts/swag-create.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+docker create\
+  --name=swag\
+  --hostname=swag\
+  --network=le_bridge\
+  --cap-add=NET_ADMIN\
+  -e PUID=1001\
+  -e PGID=1000\
+  -e TZ=Europe/London\
+  -e URL=clouddrive.hopto.org\
+  -e VALIDATION=http\
+  -e EMAIL=enisbaty@gmail.com\
+  -e DHLEVEL=2048\
+  -e ONLY_SUBDOMAINS=false\
+  -e STAGING=false\
+  -p 443:443\
+  -p 80:80\
+  -v /home/sysadmin/docker_config/swag:/config\
+  --restart=unless-stopped\
+  ghcr.io/linuxserver/swag
