3909178b1aacd938f390be0eab785bec2d62045f
Enis Baty <32305538+enisbaty@users.noreply.github.com>
2021-02-15 22:07:34 +0000

    Use jackettVPN container instead of vanilla

--- scripts/jackett-vpn-create.sh
+++ scripts/jackett-vpn-create.sh
@@ -6,12 +6,19 @@ docker create\
     -e PGID=1000\
     -e TZ=Europe/London\
     -e AUTO_UPDATE=true\
+    -e VPN_ENABLED=yes\
+    -e VPN_TYPE=openvpn\
+    -e LAN_NETWORK=192.168.1.0/24\
+    -e VPN_USERNAME=7586940515220274\
+    -e VPN_PASSWORD=m\
     -p 9117:9117\
     -v /home/sysadmin/docker-data/jackett:/config\
     --network=le_bridge\
     --log-driver json-file\
+    --cap-add=NET_ADMIN\
+    --device /dev/net/tun:/dev/net/tun\
     --log-opt max-size=10m\
     --log-opt max-file=5\
     --restart=unless-stopped\
-    ghcr.io/linuxserver/jackett
+    dyonr/jackettvpn
 
